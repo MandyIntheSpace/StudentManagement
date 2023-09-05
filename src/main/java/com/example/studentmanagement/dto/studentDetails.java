5d3362d18bb54c45793fe3fc301058ebb95d4ff6
MandyIntheSpace <timsinarewon@gmail.com>
2023-09-05 14:22:08 +0545

    RR

---
+++ src/main/java/com/example/studentmanagement/dto/studentDetails.java
@@ -0,0 +1,32 @@
+package com.example.studentmanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class studentDetails {
+
+    private String firstName;
+
+    private String gender;
+
+    private String status;
+
+    private String address;
+
+    private String maritialStatus;
+
+    private String contact;
+
+    private String addedBy;
+
+    private String modifiedBy;
+
+    private String enrolledYear;
+
+}
